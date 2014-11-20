6362427396d0c9ea004053cdbad9cea8194690dd
ariose <ariose@live.com>
2014-11-21 00:39:34 +0800

    donw

--- scripts/web-server.js
+++ scripts/web-server.js
@@ -10,8 +10,6 @@ var fs = require('fs');
 var app = express();
 
 var appbase = path.join(__dirname , '..', 't');
-// var GLOBAL_CONFIG = JSON.parse(fs.readFileSync( path.join(process.cwd() , 'package.json') ));
-// GLOBAL_CONFIG.version = parseFloat(GLOBAL_CONFIG.version);
 
 app.engine('tpl' , swig.renderFile);
 app.set('view engine' , 'tpl');
